242f3b750dc79d12bc8f1e4ac1d10130f1028816
nvTwistt <mtamkee@icloud.com>
2021-07-20 17:55:08 +0000

    fixed money formatting to add $ symbol

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -20,7 +20,7 @@ class CartsController < ApplicationController
   private
 
   def modify_cart_delta(product_id, delta)
-    cart[product_id] = (cart[product_id] || 0) + delta
+    cart[product_id] = (cart[product_id] || 00) + delta
     cart.delete(product_id) if cart[product_id] < 1
     update_cart cart
   end
