61ca40ed274cfc994d649b15216732294e7c8b89
nv_twistt <mtamkee@icloud.com>
2021-07-22 10:43:32 -0700

    added a new route for admin categories, added new class as a controller which I used the products_controller as reference

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,18 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASSWORD"]
+  def index
+    @categories = Category.order(:name).all
+  end
+
+  def new
+    @category = Category.new
+  end
+  def create
+    @category = Category.new(params.require(:category).permit(:name))
+    if @category.save
+      redirect_to [:admin, :categories], notice: "Category created!"
+    else
+      render :new
+    end
+  end
+end
